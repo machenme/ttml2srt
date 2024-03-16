d3f8727fb25b897722351a59c6dc95aa9a8c3eda
ma chen <117664524+machenme@users.noreply.github.com>
2024-03-17 02:47:21 +0800

    Create ttml2srt_cn.py

---
+++ ttml2srt_cn.py
@@ -0,0 +1,45 @@
+import argparse
+import re
+
+# pip install opencc-python-reimplemented
+from opencc import OpenCC
+
+
+def ttml_to_srt(ttml_text):
+    pattern = r'<p begin="(\d{2}:\d{2}:\d{2}.\d{3})" end="(\d{2}:\d{2}:\d{2}.\d{3})".*>(.*)<\/p>'
+    result = ""
+    for idx, sub in enumerate(ttml_text, start=1):
+        matches = re.findall(pattern, sub)
+        for match in matches:
+            begin, end, srt_sub = match
+            # 将繁体字转换为简体字
+            srt_sub = converter.convert(srt_sub)
+            result += f"{idx}\n{begin} --> {end}\n{srt_sub}\n"
+    result = result.replace(".", ",")
+    return result
+
+
+def main(input_file, output_file=None):
+    with open(input_file, "r", encoding="utf-8") as f:
+        subs = f.readlines()
+    subs = [i for i in subs if "begin" in i]
+
+    srt_content = ttml_to_srt(subs)
+
+    if output_file is None:
+        output_file = input_file.replace(".ttml", ".srt")
+
+    with open(output_file, "w+", encoding="utf-8") as f:
+        f.write(srt_content)
+
+
+if __name__ == "__main__":
+    # 创建 OpenCC 实例，将繁体字转换为简体字
+    converter = OpenCC("t2s")
+
+    parser = argparse.ArgumentParser(description="Convert TTML to SRT format")
+    parser.add_argument("input_file", help="Input TTML file")
+    parser.add_argument("-o", "--output_file", help="Output SRT file")
+    args = parser.parse_args()
+
+    main(args.input_file, args.output_file)
